7f30d09e71eb0a263226d370832d269181f24e68
vherman <vherman@globalgiving.org>
2024-12-14 12:28:34 +0200

    fix tests

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,6 +1,14 @@
-import { App } from 'aws-cdk-lib';
+import { App, Stack, StackProps } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
-import { MyStack } from '../src/main';
+import { Construct } from 'constructs';
+
+export class MyStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps = {}) {
+    super(scope, id, props);
+
+    // define resources here...
+  }
+}
 
 test('Snapshot', () => {
   const app = new App();
