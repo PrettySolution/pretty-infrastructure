0395144bb453dc21f529019b612940f3360074a5
Vasyl Herman <57800300+PrettySolution@users.noreply.github.com>
2024-12-15 20:00:23 +0200

    clean code (#16)

--- src/github-pipeline/PipelineStack.ts
+++ src/github-pipeline/PipelineStack.ts
@@ -1,6 +1,6 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { ShellStep } from 'aws-cdk-lib/pipelines';
-import { AwsCredentials, GitHubWorkflow, JobSettings, JsonPatch } from 'cdk-pipelines-github';
+import { AwsCredentials, GitHubWorkflow, JsonPatch } from 'cdk-pipelines-github';
 import { Construct } from 'constructs';
 import { MyAppStage } from './MyAppStage';
 import { GH_SUPPORT_DEPLOY_ROLE_NAME, PRIMARY_REGION, PROD_ACCOUNT, STAGE_ACCOUNT } from '../constants';
@@ -24,33 +24,28 @@ export class PipelineStack extends Stack {
     const gitHubWave = pipeline.addGitHubWave('prodWave');
 
     // STAGE
-    const stageJobSettings: JobSettings = { if: 'github.ref == \'refs/heads/main\'' };
     const stage = new MyAppStage(this, 'stage', {
       env: {
         account: STAGE_ACCOUNT,
         region: PRIMARY_REGION,
         domainName: 'stage.pretty-solution.com',
       },
-      jobSettings: stageJobSettings,
     });
     gitHubWave.addStageWithGitHubOptions(stage, {
-      jobSettings: stageJobSettings,
+      jobSettings: { if: 'github.ref == \'refs/heads/main\'' },
     });
 
     // PROD
-    const prodJobSettings: JobSettings = { if: 'github.ref == \'refs/heads/prod\'' };
-    pipeline.workflowFile.patch(JsonPatch.add('/on/push/branches/', 'prod'));
     const prod = new MyAppStage(this, 'prod', {
       env: {
         account: PROD_ACCOUNT,
         region: PRIMARY_REGION,
         domainName: 'pretty-solution.com',
       },
-      jobSettings: prodJobSettings,
     });
     gitHubWave.addStageWithGitHubOptions(prod, {
-      jobSettings: prodJobSettings,
+      jobSettings: { if: 'github.ref == \'refs/heads/prod\'' },
     });
-
+    pipeline.workflowFile.patch(JsonPatch.add('/on/push/branches/', 'prod'));
   }
 }
