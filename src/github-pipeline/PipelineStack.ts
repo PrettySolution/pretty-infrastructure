7ae0f25b36619f960b69e2c7f853154a2c3d1e17
Vasyl Herman <57800300+PrettySolution@users.noreply.github.com>
2024-12-15 19:48:27 +0200

    set up stage and prod (#15)

--- src/github-pipeline/PipelineStack.ts
+++ src/github-pipeline/PipelineStack.ts
@@ -1,6 +1,6 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { ShellStep } from 'aws-cdk-lib/pipelines';
-import { AwsCredentials, GitHubWorkflow, JsonPatch } from 'cdk-pipelines-github';
+import { AwsCredentials, GitHubWorkflow, JobSettings, JsonPatch } from 'cdk-pipelines-github';
 import { Construct } from 'constructs';
 import { MyAppStage } from './MyAppStage';
 import { GH_SUPPORT_DEPLOY_ROLE_NAME, PRIMARY_REGION, PROD_ACCOUNT, STAGE_ACCOUNT } from '../constants';
@@ -21,32 +21,36 @@ export class PipelineStack extends Stack {
         gitHubActionRoleArn: `arn:aws:iam::${PROD_ACCOUNT}:role/${GH_SUPPORT_DEPLOY_ROLE_NAME}`,
       }),
     });
-
-    // PROD
-    const prod = new MyAppStage(this, 'prod', {
-      env: {
-        account: PROD_ACCOUNT,
-        region: PRIMARY_REGION,
-        domainName: 'pretty-solution.com',
-      },
-    });
     const gitHubWave = pipeline.addGitHubWave('prodWave');
-    gitHubWave.addStageWithGitHubOptions(prod, {
-      jobSettings: { if: 'github.ref == \'refs/heads/main\'' },
-    });
 
     // STAGE
+    const stageJobSettings: JobSettings = { if: 'github.ref == \'refs/heads/main\'' };
     const stage = new MyAppStage(this, 'stage', {
       env: {
         account: STAGE_ACCOUNT,
         region: PRIMARY_REGION,
         domainName: 'stage.pretty-solution.com',
       },
+      jobSettings: stageJobSettings,
     });
     gitHubWave.addStageWithGitHubOptions(stage, {
-      jobSettings: { if: 'github.ref == \'refs/heads/stage\'' },
+      jobSettings: stageJobSettings,
     });
-    pipeline.workflowFile.patch(JsonPatch.add('/on/push/branches/', 'stage'));
+
+    // PROD
+    const prodJobSettings: JobSettings = { if: 'github.ref == \'refs/heads/prod\'' };
+    pipeline.workflowFile.patch(JsonPatch.add('/on/push/branches/', 'prod'));
+    const prod = new MyAppStage(this, 'prod', {
+      env: {
+        account: PROD_ACCOUNT,
+        region: PRIMARY_REGION,
+        domainName: 'pretty-solution.com',
+      },
+      jobSettings: prodJobSettings,
+    });
+    gitHubWave.addStageWithGitHubOptions(prod, {
+      jobSettings: prodJobSettings,
+    });
 
   }
 }
